ee7d50f3c6f7582975102bdbb764c477079d1f0a
Ryzen777888 <kosyakravchuck1@gmail.com>
2022-01-20 19:51:59 +0300

    Initial commit

---
+++ calculator2.1/src/store.js
@@ -0,0 +1,13 @@
+let store = {
+    operations:[
+        {val:`C`}, {val:`CE`},{val:`=`}
+    ],
+    buttons :[
+        {val:`/`}, {val:`7`}, {val:`8`}, {val:`9`},
+        {val:`*`}, {val:`4`}, {val:`5`}, {val:`6`}, {val:`-`},
+        {val:`1`}, {val:`2`},{val:`3`},{val:`+`},
+        {val:`.`},{val:`0`},
+    ],
+
+}
+export default store;
